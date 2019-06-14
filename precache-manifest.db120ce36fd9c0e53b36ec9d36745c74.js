ef45a6fc6bc72ca10fe32ecdbe212be52c03b41d
Ros <newxpyct@gmail.com>
2019-06-14 15:59:36 +0300

    Updates

---
+++ precache-manifest.db120ce36fd9c0e53b36ec9d36745c74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe60dc5315509ce8f04c14dc90772a5f",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "c12210236b5ed2a7d13d",
+    "url": "/calendar/static/css/main.631c959e.chunk.css"
+  },
+  {
+    "revision": "8d5ccb9ca459f0a51efe",
+    "url": "/calendar/static/js/2.548bac30.chunk.js"
+  },
+  {
+    "revision": "c12210236b5ed2a7d13d",
+    "url": "/calendar/static/js/main.fcc1bd49.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
